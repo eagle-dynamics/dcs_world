eddbf7c1481752293b682bc87e9d73964b49ba2b
R3DL1N3 <r0y@digitalcombatsimulator.world>
2020-04-06 19:20:57 +0000

    Pack author, home, download links

--- pack.pl
+++ pack.pl
@@ -1,3 +1,8 @@
 name(dcs_world).
 version('0.1.0').
 title('Eagle Dynamics DCS World').
+author('R3DL1N3', 'r0y@digitalcombatsimulator.world').
+packager('R3DL1N3', 'r0y@digitalcombatsimulator.world').
+maintainer('R3DL1N3', 'r0y@digitalcombatsimulator.world').
+home('https://github.com/eagle-dynamics/dcs_world').
+download('https://github.com/eagle-dynamics/dcs_world/releases/*.zip').
