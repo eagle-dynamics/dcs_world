4c3698a95aea5866eaa943788ad0a63b317529e3
R3DL1N3 <r0y@digitalcombatsimulator.world>
2020-04-17 16:28:15 +0000

    Point dimensions, northing, easting, altitude

--- prolog/dcs_world/points.pl
+++ prolog/dcs_world/points.pl
@@ -15,3 +15,21 @@ dcs:property_of_point(point(X, Y), Point) :-
     dcs:property_of_point(terrain(_, point(X, Y)), Point).
 dcs:property_of_point(point(X, Y, Z), Point) :-
     dcs:property_of_point(terrain(_, point(X, Y, Z)), Point).
+
+dcs:property_of_point(dimensions(2), Point) :-
+    dcs:property_of_point(point(_, _), Point).
+dcs:property_of_point(dimensions(3), Point) :-
+    dcs:property_of_point(point(_, _, _), Point).
+
+dcs:property_of_point(northing(Northing), Point) :-
+    dcs:property_of_point(point(Northing, _), Point).
+dcs:property_of_point(northing(Northing), Point) :-
+    dcs:property_of_point(point(Northing, _, _), Point).
+
+dcs:property_of_point(easting(Easting), Point) :-
+    dcs:property_of_point(point(_, Easting), Point).
+dcs:property_of_point(easting(Easting), Point) :-
+    dcs:property_of_point(point(_, _, Easting), Point).
+
+dcs:property_of_point(altitude(Altitude), Point) :-
+    dcs:property_of_point(point(_, Altitude, _), Point).
