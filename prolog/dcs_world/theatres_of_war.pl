6fac01f2e35579a3da2a19b3c6f736509274d17c
R3DL1N3 <r0y@digitalcombatsimulator.world>
2020-04-18 09:55:18 +0000

    Module description ported from core

--- prolog/dcs_world/theatres_of_war.pl
+++ prolog/dcs_world/theatres_of_war.pl
@@ -1,5 +1,27 @@
 :- module(dcs_world_theatres_of_war, [theatre_of_war_property/2]).
 
+/** <module> Theatres of war
+ *
+ * What is a theatre of war? Theatres  of war align with terrains. They
+ * are  not  terrains,   however.   Theatres    comprise:   a   terrain
+ * configuration, beacons and nodes. As an   example, the following are
+ * the parameters for the Caucasus "theatre  of war." Excludes the long
+ * theatre description.
+ *
+ * |   beacons_file    |         "./Mods/terrains/Caucasus\\Beacons.lua"         |
+ * |  config_filename  |       "./Mods/terrains/Caucasus\\terrain.cfg.lua"       |
+ * |config_filename_dll|     "./Mods/terrains/Caucasus\\terrain.cfg.lua.dll"     |
+ * |      folder       |              "./Mods/terrains/Caucasus\\"               |
+ * |       image       |           "./Mods/terrains/Caucasus\\map.png"           |
+ * |  localized_name   |                       "Caucasus"                        |
+ * |       name        |                       "Caucasus"                        |
+ * |    nodes_file     | "./Mods/terrains/Caucasus\\MissionGenerator/nodes.lua"  |
+ * | nodes_map_borders |     [-418619.1875,113728.15625,26382.5,943187.0625]     |
+ * |  nodes_map_file   |"./Mods/terrains/Caucasus\\MissionGenerator/nodesMap.png"|
+ *
+ * @author R3DL1N3 <r0y@digitalcombatsimulator.world>
+ */
+
 :- multifile dcs:property_of_theatre_of_war/2.
 
 %!  theatre_of_war_property(?TheatreOfWar:atom, ?Property) is nondet.
