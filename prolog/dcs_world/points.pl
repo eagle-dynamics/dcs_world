4796ce61974c794d603ded7cf624580ef1688071
R3DL1N3 <r0y@digitalcombatsimulator.world>
2020-05-01 04:19:20 +0000

    Comments for points

--- prolog/dcs_world/points.pl
+++ prolog/dcs_world/points.pl
@@ -1,11 +1,33 @@
 :- module(dcs_world_points, [point_property/2]).
 
-/** <module> Somewhere within a theatre
+/** <module> Somewhere within a theatre of war
+ *
+ * Points are locations within a theatre of   war.  In other words, they
+ * specify one defined theatre of war   plus  a coordinate. Points exist
+ * within  worlds.  Vectors  exist  within  variants.  Worlds  translate
+ * between  point  and  vector  when  communicating  between  world  and
+ * variant. Points have two varieties:  two- or three-dimensional. Point
+ * is a compound of the form Terrain/2 or Terrain/3 where Terrain is one
+ * of terrain_property(Terrain, defined).
  *
  * @author R3DL1N3 <r0y@digitalcombatsimulator.world>
  */
 
 :- multifile dcs:property_of_point/2.
+
+%!  point_property(+Point, ?Property) is nondet.
+%
+%   Property of Point.
+%
+%       * terrain(Terrain, point(X, Y))
+%       * terrain(Terrain, point(X, Y, Z))
+%       * terrain(Terrain)
+%       * point(X, Y)
+%       * point(X, Y, Z)
+%       * northing(Northing)
+%       * easting(Easting)
+%       * altitude(Altitude)
+%       * dimensions(Dimensions)
 
 point_property(Point, Property) :-
     dcs:property_of_point(Property, Point).
