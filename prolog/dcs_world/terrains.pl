746a082fe9c055fc031bffbe6e71a91c468048fa
R3DL1N3 <r0y@digitalcombatsimulator.world>
2020-05-08 08:50:17 +0000

    Refactored terrains/of_config; includes airdrome terms

--- prolog/dcs_world/terrains.pl
+++ prolog/dcs_world/terrains.pl
@@ -61,37 +61,8 @@ property_of_point(point(X, Y), [X, Y]).
 property_of_point(point(X, Y, Z), [X, Y, Z]).
 
 dcs:property_of_terrain(config:Config, Terrain) :-
-    config_of_terrain(Config, Terrain).
+    of_config(Config, Terrain).
 
-config_of_terrain(id("Caucasus"),caucasus).
-config_of_terrain(bound(sw,[-600,0,-560]),caucasus).
-config_of_terrain(bound(ne,[380,0,1130]),caucasus).
-config_of_terrain(camera(default,[-355,0.2,618]),caucasus).
-config_of_terrain(bullseye(default,blue(x(-291014))),caucasus).
-config_of_terrain(bullseye(default,blue(y(617414))),caucasus).
-config_of_terrain(bullseye(default,red(x(11557))),caucasus).
-config_of_terrain(bullseye(default,red(y(371700))),caucasus).
-config_of_terrain(id("Nevada"),nevada).
-config_of_terrain(bound(sw,[-600,0,-450]),nevada).
-config_of_terrain(bound(ne,[0,0,225]),nevada).
-config_of_terrain(camera(default,[-288.62,1.637,-88.323]),nevada).
-config_of_terrain(bullseye(default,blue(x(-409931.344))),nevada).
-config_of_terrain(bullseye(default,blue(y(-14024.097))),nevada).
-config_of_terrain(bullseye(default,red(x(-288293.969))),nevada).
-config_of_terrain(bullseye(default,red(y(-88022.641))),nevada).
-config_of_terrain(id("Normandy"),normandy).
-config_of_terrain(bound(sw,[-130,0,-280]),normandy).
-config_of_terrain(bound(ne,[220,0,190]),normandy).
-config_of_terrain(camera(default,[-12,0.8,-46]),normandy).
-config_of_terrain(bullseye(default,blue(x(-11938.056641))),normandy).
-config_of_terrain(bullseye(default,blue(y(-47277.990234))),normandy).
-config_of_terrain(bullseye(default,red(x(-45898.337891))),normandy).
-config_of_terrain(bullseye(default,red(y(112233.773438))),normandy).
-config_of_terrain(id("PersianGulf"),persian_gulf).
-config_of_terrain(bound(sw,[-460,0,-900]),persian_gulf).
-config_of_terrain(bound(ne,[800,0,800]),persian_gulf).
-config_of_terrain(camera(default,[67.35,0.3,-35.61]),persian_gulf).
-config_of_terrain(bullseye(default,blue(x(0))),persian_gulf).
-config_of_terrain(bullseye(default,blue(y(0))),persian_gulf).
-config_of_terrain(bullseye(default,red(x(0))),persian_gulf).
-config_of_terrain(bullseye(default,red(y(0))),persian_gulf).
+:- table of_config/2 as shared.
+
+:- include(terrains/of_config).
