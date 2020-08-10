f2912e7ae2ae39f6bf23b0419eaffe0e2ee1bf6e
R3DL1N3 <r0y@digitalcombatsimulator.world>
2020-08-10 06:20:38 +0000

    Refactor start_up_server/1, wait_for_server/1

--- prolog/dcs_world/terrain/airdrome_beta.pl
+++ prolog/dcs_world/terrain/airdrome_beta.pl
@@ -1,33 +1,11 @@
-:- [library(dcs_world/beta)].
-
-:- ensure_loaded(library(ed/apps)).
-:- ensure_loaded(library(ed/logs)).
-:- ensure_loaded(library(ed/windows)).
-:- ensure_loaded(library(ed/dispatchers)).
-
-:- ensure_loaded(library(ed/terrains)).
-:- ensure_loaded(library(ed/theatres_of_war)).
-
-:- [library(os/apps_debugging)].
-:- [library(canny/situations_debugging)].
-:- [library(dcs/logs_debugging)].
+:- [beta].
 
 :- initialization with_output_to_pl(airdrome, all).
 
 all :-
-    once(up(Variant)),
-    repeat,
-    sleep(1),
-    variant_property(Variant, server),
-    variant_property(Variant, dispatcher:state(3)),
-    !,
+    wait_for_server(Variant),
     forall(each(Variant, Term), print_term(Term)).
 
 each(Variant, airdrome(Property, Airdrome)) :-
     variant_apply(Variant, theatre_of_war(_)),
     variant_property(Variant, terrain:airdrome(Airdrome, Property)).
-
-up(Variant) :-
-    variant_property(Variant, defined),
-    \+ variant_property(Variant, server),
-    variant_apply(Variant, up).
---
+++ prolog/dcs_world/terrain/beta.pl
@@ -0,0 +1,32 @@
+:- [library(dcs_world/beta)].
+
+:- ensure_loaded(library(ed/apps)).
+:- ensure_loaded(library(ed/logs)).
+:- ensure_loaded(library(ed/windows)).
+:- ensure_loaded(library(ed/dispatchers)).
+
+:- ensure_loaded(library(ed/terrains)).
+:- ensure_loaded(library(ed/theatres_of_war)).
+
+:- [library(os/apps_debugging)].
+:- [library(canny/situations_debugging)].
+:- [library(dcs/logs_debugging)].
+
+:- initialization start_up_server(_).
+
+start_up_server(Variant) :- server_is_ready(Variant), !.
+start_up_server(Variant) :-
+    variant_property(Variant, defined),
+    \+ variant_property(Variant, server),
+    variant_apply(Variant, up),
+    !.
+
+wait_for_server(Variant) :-
+    repeat,
+    sleep(1),
+    server_is_ready(Variant),
+    !.
+
+server_is_ready(Variant) :-
+    variant_property(Variant, server),
+    variant_property(Variant, dispatcher:state(3)).
