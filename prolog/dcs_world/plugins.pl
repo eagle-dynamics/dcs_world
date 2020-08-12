297d610e33a9e81a080a92d4e199d3a6c2c225d8
R3DL1N3 <r0y@digitalcombatsimulator.world>
2020-08-12 06:26:12 +0000

    Plug-ins by identifier using soft-cut

--- prolog/dcs_world/plugins.pl
+++ prolog/dcs_world/plugins.pl
@@ -1,11 +1,32 @@
 :- module(dcs_world_plugins, [plugin_property/2]).
 
-of_plugin(defined, Plugin) :- of_plugin(id(_), Plugin).
+:- multifile dcs:property_of_plugin/2.
 
-:- include(plugins/of_plugin).
+:- include(plugins_by_id).
 
 %!  plugin_property(?Plugin, ?Property) is nondet.
 %
 %   Property of Plugin.
+%
+%       * defined
+%
+%       This implementation of defined of Plugin highlights an important
+%       issue when dealing with  the   soft-cut  approach  for multifile
+%       extensions  to  properties.  It  performs   a  recursive  query.
+%       Fundamental facts require  a   plugin_property/2  predicate call
+%       rather than dcs:property_of_plugin/2 because the latter does not
+%       query the fundamental  facts;  instead,  it   only  asks  for  a
+%       derivative. This requires that the   invoker know which Property
+%       has a fundamental nature, and which has a derived one.
+%
+%       The upshot? You find all  the   fundamentals  using the ordinary
+%       predicate  and  all  the  derived  facts  using  the  multi-file
+%       predicate. Same principle applies throughout the DCS World pack.
 
-plugin_property(Plugin, Property) :- of_plugin(Property, Plugin).
+plugin_property(Plugin, Property) :-
+    (   plugins_by_id(Property, Plugin)
+    *-> true
+    ;   dcs:property_of_plugin(Property, Plugin)
+    ).
+
+dcs:property_of_plugin(defined, Plugin) :- plugin_property(Plugin, id(_)).
---
+++ prolog/dcs_world/plugins_by_id_gui.pl
@@ -0,0 +1,13 @@
+:- [library(dcs_world/scripts/gui)].
+
+:- initialization with_output_to_pl(plugins_by_id, all).
+
+all :- forall(each(Term), print_term(Term)).
+
+each(plugins_by_id(Property, ID)) :-
+    gui_apply(gui(pluginsById, Plugins)),
+    is_dict(Plugins),
+    dict_pairs(Plugins, _, Pairs),
+    member(ID0-Plugin, Pairs),
+    restyle_identifier(ID0, ID),
+    dict_compound(Plugin, Property).
